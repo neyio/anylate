fee53cf01623a57d2a345220979305d89548afd9
Neyio <neyioqian@gmail.com>
2019-12-02 15:54:46 +0800

    发版

--- packages/slate-md-support/lib/slate-md-support.es.js
+++ packages/slate-md-support/lib/slate-md-support.es.js
@@ -1,2 +1,6 @@
+var hello2 = function hello2() {
+  return 'hello world2';
+};
 
+export { hello2 };
 //# sourceMappingURL=slate-md-support.es.js.map
--- packages/slate-md-support/lib/slate-md-support.js
+++ packages/slate-md-support/lib/slate-md-support.js
@@ -1,3 +1,10 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var hello2 = function hello2() {
+  return 'hello world2';
+};
+
+exports.hello2 = hello2;
 //# sourceMappingURL=slate-md-support.js.map
